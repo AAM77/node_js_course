0d04b6694aa5165b301d6ca4fb2ed76dea4c62b6
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-09-12 04:46:59 -0400

    learn more about running script from other files using require.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,5 @@
-const fs = require('fs');
+require('./utils.js');
 
-const msg = 'This file was created by Node.js. The file is meant to help understand the module system for fs commands.';
-const appendedMsg = '\nThis is the appended message.'
+const name = 'Adeel';
 
-// fs.writeFileSync('notes.txt', msg);
-fs.appendFileSync('notes.txt', appendedMsg);
+console.log(name);
---
+++ notes-app/utils.js
@@ -0,0 +1,3 @@
+// this file contains some useful utilities for the app
+
+console.log('utils.js');
