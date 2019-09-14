83d423766f7d91387a4945c8354cd159b36f8eba
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-09-13 23:48:55 -0400

    learned how to import installed modules using require & how to then use them after assigning the import to a variable.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,11 @@
+const validator = require('validator');
 const getNotes = require('./notes.js');
 
 console.log(getNotes());
+
+
+console.log(validator.isEmail('adeel@example.com'));
+console.log(validator.isURL('https://mead.io'));
 
 // const addFunction = require('./utils.js');
 //
