9219765d8c81769458d7f98e64aef12fecf4419f
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-09-14 01:02:03 -0400

    Tested out how yargs works.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,13 +1,27 @@
 const validator = require('validator');
 const chalk = require('chalk');
+const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
+// customize yargs version
+yargs.version('1.1.0');
+
+// create add command
+
+// add, remove, read, list notes
+
 const command = process.argv[2];
+const argv = yargs.argv;
 
 if (command === 'add') {
   console.log('Adding note...');
 } else if (command === 'remove') {
   console.log('Removing note...');
+} else if (argv.title) {
+  if (argv.title === 'Happy Days') {
+    console.log(chalk.green('Happy Success!!'));
+  }
 }
 
-console.log(process.argv[3]);
+console.log(process.argv);
+console.log(yargs.argv);
