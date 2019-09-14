bda0b5ef0c4c401c91b880e58e56cdf6bea8c3bc
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-09-14 00:00:19 -0400

    imported & used/tested out chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,5 @@
 const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes.js');
 
 console.log(getNotes());
@@ -6,6 +7,8 @@ console.log(getNotes());
 
 console.log(validator.isEmail('adeel@example.com'));
 console.log(validator.isURL('https://mead.io'));
+console.log(chalk.magenta.bgWhite('Hello world!'));
+console.log(chalk.blue.bgYellow(validator.isURL('https://localhost:3000')));
 
 // const addFunction = require('./utils.js');
 //
