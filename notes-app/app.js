031d4aeef8876832101385954dbb280ec7efe1f5
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-09-14 00:19:07 -0400

    tested out some code after installing nodemon globally.

--- notes-app/app.js
+++ notes-app/app.js
@@ -9,8 +9,10 @@ console.log(validator.isEmail('adeel@example.com'));
 console.log(validator.isURL('https://mead.io'));
 console.log(chalk.magenta.bgWhite('Hello world!'));
 console.log(chalk.blue.bgYellow(validator.isURL('https://localhost:3000')));
-console.log(chalk.green.dim.bold('Success!'))
-console.log(chalk.green('I am a green line ' + chalk.blue.underline.bold('with a blue substring') + ' that becomes green again!'));
+console.log(chalk.green.dim.bold('Success!'));
+console.log(chalk.red.dim.bold('Error!'));
+console.log(chalk.green.dim.bold('Success!'));
+console.log(chalk.green('I am a green line ' + chalk.yellow.underline.bold('with a yellow substring') + ' that becomes green again!'));
 
 // const addFunction = require('./utils.js');
 //
