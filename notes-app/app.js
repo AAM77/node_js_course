4941afe074b3dcdb3d55799d24a35949138f2494
Mohammad Adeel (AAM77) <Adeel.A.Mhd@gmail.com>
2019-09-14 17:53:14 -0400

    created some commands using yargs.

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,4 @@
-const validator = require('validator');
+// const validator = require('validator');
 const chalk = require('chalk');
 const yargs = require('yargs');
 const getNotes = require('./notes.js');
@@ -6,22 +6,58 @@ const getNotes = require('./notes.js');
 // customize yargs version
 yargs.version('1.1.0');
 
+
 // create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: function () {
+    console.log('Adding a new note!');
+  }
+})
+
+// create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+    console.log('Removing the note');
+  }
+})
+
+// create list command
+
+yargs.command({
+  command: 'list',
+  describe: 'List the notes',
+  handler: function () {
+    console.log('Listing all notes');
+  }
+})
+
+// create read command
+
+yargs.command({
+  command: 'read',
+  describe: 'Read the note',
+  handler: function () {
+    console.log('Reading the note');
+  }
+})
 
 // add, remove, read, list notes
-
-const command = process.argv[2];
-const argv = yargs.argv;
-
-if (command === 'add') {
-  console.log('Adding note...');
-} else if (command === 'remove') {
-  console.log('Removing note...');
-} else if (argv.title) {
-  if (argv.title === 'Happy Days') {
-    console.log(chalk.green('Happy Success!!'));
-  }
-}
-
-console.log(process.argv);
 console.log(yargs.argv);
+
+
+// if (command === 'add') {
+//   console.log('Adding note...');
+// } else if (command === 'remove') {
+//   console.log('Removing note...');
+// } else if (argv.title) {
+//   if (argv.title === 'Happy Days') {
+//     console.log(chalk.green('Happy Success!!'));
+//   }
+// }
+//
+// console.log(process.argv);
+// console.log(yargs.argv);
